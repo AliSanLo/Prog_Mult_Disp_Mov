df0a7495cb21419f1c48bb1b6c41fd839d8ad73d
Alicia Sánchez <alicia.sl@hotmail.es>
2024-01-03 18:07:25 +0100

    .
    .

--- Estudio/Clase_3/Clase3.1/Practica_2/app/src/main/java/com/example/practica_2/MainActivity.kt
+++ Estudio/Clase_3/Clase3.1/Practica_2/app/src/main/java/com/example/practica_2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.practica_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
@@ -15,10 +17,25 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var botonIncremento: Button
     private lateinit var botonDecremento: Button
     private lateinit var textoContador: TextView
+    private  var botonPasar : Button? = null
     private var  contador : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*el savedInstanceState es donde se guarda el 'Estado de la Instancia Guardado'
+        Se utiliza para enviar los datos de la aplicacion al layout horizontal
+        bundle tiene '?' porque puede ser nulo. Por ejemplo al ejecutar la aplicacion de 0
+        puede estar vacía sin datos que guardar, osea, nulo. */
+
         super.onCreate(savedInstanceState)
+
+        Log.v ("ciclo", "ejecutando onCreate")
+
+        //si se rompe la aplicacion, hay que recuperar los datos:
+       contador = savedInstanceState?.getInt("contador")  ?:0
+        /*da error, y se añade el '?' y un valor por defecto -> ?:=
+        de esta forma si savedInstanceState es nulo salta el get int al ?:0. A esto se le llama operacion condicional,
+        pero no es lo mismo que un if ternario.*/
+        //una ve recuperado el estado:
 
 
         // asociar la parte logica a la parte grafica
@@ -28,10 +45,14 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         botonDecremento = findViewById(R.id.boton_decremento)
         botonIncremento = findViewById(R.id.boton_incremento)
         textoContador = findViewById(R.id.texto_contador)
+        botonPasar = findViewById(R.id.boton_pasar)
+        textoContador.text = contador.toString(); // porque no pueden ponerse nmeros en un texto(?)
 
         // decir a los botones que tienen que escuchar algo
         botonIncremento.setOnClickListener(this) // this -> polimorfismo
         botonDecremento.setOnClickListener(this)
+        botonPasar?.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View?) { //se crea automaticamente al implementar OnClickListener
@@ -46,8 +67,23 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             R.id.boton_decremento -> {
                 contador--
             }
+
+                R.id.boton_pasar ->{
+
+                }
+
         }
             textoContador.text = contador.toString()
 
     }
-}
+
+    override fun onSaveInstanceState(outState: Bundle) { //este bundle no es nulo porque siempre tiene que guadar algo
+        super.onSaveInstanceState(outState)
+
+        //para clave-valor
+        outState.putInt("Contador", contador)
+
+        }
+    }
+
+
---
+++ Estudio/Clase_3/Clase3.1/README.md.txt
@@ -0,0 +1,9 @@
+Cositas que hay en la Practica_2 para repasar: 
+	- interfaz SetOnClickListener y metodo el OnClick
+	- Declaracion de variables atribuidas a elementos graficos del programa
+		Ej.:   botonDecremento = findViewById(R.id.boton_decremento)
+	- Ciclo de vida de una pantalla, como guardar su estado y como recuperarlo con la 
+	    explicacion del savedInstanceState y onSavedInstanceState
+	- una operacion condicional
+	- android:layout_weight="0.3"
+	- tercer boton en horizontal (sin funcion)
