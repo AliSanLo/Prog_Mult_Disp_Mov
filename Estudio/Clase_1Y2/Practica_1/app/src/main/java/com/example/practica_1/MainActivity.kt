c7302a07d7139a461eb1c28f5a436510d6c3866b
Alicia Sánchez <alicia.sl@hotmail.es>
2024-01-03 10:08:51 +0100

    .
    .

--- Estudio/Clase_1Y2/Practica_1/app/src/main/java/com/example/practica_1/MainActivity.kt
+++ Estudio/Clase_1Y2/Practica_1/app/src/main/java/com/example/practica_1/MainActivity.kt
@@ -8,8 +8,8 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
     /*Declaración de la variable botonPulsar como lateinit:
     lateinit se utiliza para indicar que la inicialización de la variable se realizará en algún momento después
-    de la declaración, pero antes de que se utilice. En este caso, el botón se inicializará más adelante en el método onCreate.
-    lateinit var botonPulsar : Button; //lateiniit porque hasta que no se asocie mas abajo, no funciona*/
+    de la declaración, pero antes de que se utilice. En este caso, el botón se inicializará más adelante en el método onCreate.*/
+    lateinit var botonPulsar : Button; //lateiniit porque hasta que no se asocie mas abajo, no funciona
 
     /*Método onCreate:
     * *Este método se llama cuando la actividad está siendo creada. En él, se establece el contenido de la actividad con
@@ -29,6 +29,20 @@ class MainActivity : AppCompatActivity() {
         * Snackbar con el mensaje "Práctica completada" y una duración corta*/
         botonPulsar.setOnClickListener {
             Snackbar.make(it, "practica completada", Snackbar.LENGTH_SHORT).show()
+            /*En Kotlin, cuando se utiliza una función de orden superior (como setOnClickListener), y se espera un parámetro
+            en esa función, puedes referenciar ese parámetro de manera implícita utilizando it.
+            it se refiere al objeto sobre el cual se está realizando la acción, es decir, al botón botonPulsar. El it es
+            implícito y se utiliza porque solo se espera un parámetro en la función setOnClickListener. Podrías haberlo
+            escrito explícitamente de la siguiente manera:
+
+                botonPulsar.setOnClickListener { view ->
+                    Snackbar.make(view, "practica completada", Snackbar.LENGTH_SHORT).show()
+
+
+            En este caso, he utilizado el nombre view en lugar de it, pero ambos son equivalentes. view es un nombre más
+            descriptivo y podría hacer que el código sea más claro para algunas personas. En resumen, it es simplemente una
+            referencia implícita al objeto sobre el cual se está actuando dentro de una función de orden superior
+            */
         }
 
 
